309d9ee0f14d69e2053e45a230cd8adcb377f43f
Skilldrick <skilldrick@gmail.com>
2011-08-27 10:59:50 +0100

    Add some hacky routing to messages-controller.

--- messages-controller.js
+++ messages-controller.js
@@ -1,18 +1,37 @@
-module.exports = function (req, res, action) {
-  action = action || 'index';
+function messageFactory(messageId) {
+  var messages = [
+    'Hello world!',
+    'Hi there',
+    'Yo dude!!'
+  ];
+  return {
+    id: messageId,
+    user_id: 1,
+    text: messages[messageId % 3]
+  };
+}
 
+module.exports = function (req, res, action, id) {
   var actions = {
     index: function () {
-      var output = [
-        {text: 'Hello world!'},
-        {text: 'Hello there!'},
-        {text: 'Yo dude!!!'}
-      ];
-      res.writeHead(200, {'Content-Type': 'text/plain'});
+      var output = [0,1,2,3,4].map(messageFactory);
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(output));
+      res.end('\n');
+    },
+    show: function (id) {
+      var output = messageFactory(id);
+      res.writeHead(200, {'Content-Type': 'application/json'});
       res.write(JSON.stringify(output));
       res.end('\n');
     }
   };
 
-  actions[action]();
+  action = action || 'index';
+  if (!(action in actions)) {
+    //if action is not an action, assume it's an id
+    id = +action;
+    action = 'show';
+  }
+  actions[action](id);
 };
